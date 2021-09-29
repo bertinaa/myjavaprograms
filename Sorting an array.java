fb44b4f5e1294e0a3370c2f13888e9833266adfb
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-09-29 21:25:08 +0530

    Create Sorting an array.java

---
+++ Sorting an array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+//java program to find largest and second largest number
+class Main{
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int i, j, temp;
+        int arr[] = new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        
+        System.out.print("The last element is "+ arr[n-1] +"\n");
+        System.out.print("The second last element is "+ arr[n-2]);
+    }
+}
