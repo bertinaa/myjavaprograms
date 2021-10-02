e1dda0a703c8ad302c86c5e1c40ab2055d53eab0
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-10-02 10:31:50 +0530

    Create reverse a string.java

---
+++ reverse a string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        char[] ch = str.toCharArray();
+        int n = ch.length;
+        for(int i=n-1;i>=0;i--){
+            System.out.print(ch[i]);
+  
+}
+}
+}
